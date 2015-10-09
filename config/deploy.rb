942572c87f6cb2384caf19b488597c9366913cf2
ericaROR <eserica.ror@gmail.com>
2015-10-09 14:37:47 +0545

    production added in database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :linked_files, %w{config/database.yml}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+ set :keep_releases, 5
 
 namespace :deploy do
 
