0c5c4da34eb6855f554d365255cb946d08e802af
bipashant <bipashant@gmail.com>
2015-11-16 17:58:25 +0545

    Adaptive Checkout integrated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,11 +124,17 @@ Kassi::Application.configure do
 
   config.after_initialize do
     ActiveMerchant::Billing::Base.mode = :test
+    # paypal_options = {
+    #     login: APP_CONFIG.paypal_username,
+    #     password: APP_CONFIG.paypal_password,
+    #     signature: APP_CONFIG.paypal_signature,
+    #     appid: APP_CONFIG.paypal_app_id
+    # }
     paypal_options = {
-        login: APP_CONFIG.paypal_username,
-        password: APP_CONFIG.paypal_password,
-        signature: APP_CONFIG.paypal_signature,
-        appid: APP_CONFIG.paypal_app_id
+        login: "vikas-facilitator_api1.esignature.com.np",
+        password: "1395372424",
+        signature: "An5ns1Kso7MWUdW4ErQKJJJ4qi4-AeUWvcYwAah4uQWmMBoHdxQ52OhD",
+        appid: "APP-80W284485P519543T"
     }
     ::ADAPTIVE_GATEWAY = ActiveMerchant::Billing::PaypalAdaptivePayment.new(paypal_options)
 
